77c9f515fae036bd013ec50660dfa91f36092af4
Georgina <faithwangare750@gmail.com>
2023-03-11 10:50:09 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+let a = 800;
+let b = 8;
+function add(a,b) {
+    return a + b;
+}
+let x = 30;
+let y = 7;
+function subtract(x,y) {
+    return x - y;
+}
+let p = 4;
+let q = 11799;
+function multiply(p,q) {
+    return p * q;
+}
+let c = 7199.596977329974;
+let d = 9856;
+function divide(c,d) {
+    return c / d;
+}
+let n = 3;
+function increment(n) {
+    ++n;
+    return n;
+}
+let r = 1;
+function decrement(r) {
+    --r;
+    return r;
+}
+let k = "839";
+function makeInt(k) {
+    return parseInt(k, 10);
+}
+let s = "2.222"
+function preserveDecimal(s) {
+    return parseFloat(s);
+}
